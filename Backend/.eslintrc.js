8a40233283ddd81f9383ebedca45bcf115846f2a
Emerson Fernandes <emerson_feral@hotmail.com>
2022-04-26 19:02:59 -0300

    Eslint new rules

--- Backend/.eslintrc.js
+++ Backend/.eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base'
   ],
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 13
   },
   rules: {
     'comma-dangle': [2, 'never'],
     'linebreak-style': 0,
     'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows'],
-  },
+    'eslint linebreak-style': [0, 'error', 'windows']
+  }
 };
