96a672e5352941fa77a40bd1deef31f715db0137
Emerson Fernandes <emerson_feral@hotmail.com>
2022-04-26 19:15:43 -0300

    Employee model

--- Backend/src/model/employeeModel.js
+++ Backend/src/model/employeeModel.js
@@ -1,12 +1,39 @@
-const mongoose = require('mongoose');
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('employee', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    CPF: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    avatar: {
+      type: DataTypes.STRING
+    },
+    bio: {
+      type: DataTypes.TEXT
+    },
+    senha: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
+  });
 
-const employeeSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  CPF: Number,
-  value: Number,
-  bio: String
-
-});
-
-module.exports = mongoose.model('Employee', employeeSchema);
+  return Employee;
+};
