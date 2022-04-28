b8b89cdc8cffda61405951a904f488cec86650b5
Emerson Fernandes <emerson_feral@hotmail.com>
2022-04-28 15:03:51 -0300

    Reservation model

---
+++ Backend/src/model/reservationModel.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const Reservation = sequelize.define('reservation', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    data: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    cpfResponsavelReserva: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'employees',
+        key: 'cpf'
+      }
+    },
+    idVeiculoReserva: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'vehicles',
+        key: 'id'
+      }
+    },
+    valorReserva: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    }
+
+  }, {
+    timestamps: false
+  });
+
+  return Reservation;
+};
