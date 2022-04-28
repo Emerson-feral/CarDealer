cd11d3804e177684465677bfee9bcdf85f2faab2
Emerson Fernandes <emerson_feral@hotmail.com>
2022-04-28 15:03:28 -0300

    Reservation and sales added to the config file

--- Backend/src/model/index.js
+++ Backend/src/model/index.js
@@ -32,6 +32,8 @@ db.sequelize = sequelize;
 
 db.employees = require('./employeeModel')(sequelize, DataTypes);
 db.vehicle = require('./vehicleModel')(sequelize, DataTypes);
+db.reservation = require('./reservationModel')(sequelize, DataTypes);
+db.sales = require('./salesModel')(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false })
   .then(() => {
